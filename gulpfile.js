142dc224df18c1af03c3d58bf53f08c925911652
Kevin Parrott <parrott.kevin@gmail.com>
2014-10-20 20:45:40 -0400

    Refactored for local source instead of CDN

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp'),
   gutil = require('gulp-util'),
   browserify = require('gulp-browserify'),
   concat = require('gulp-concat'),
-  // clean = require('gulp-clean'),
   del = require('del'),
   less = require('gulp-less'),
   path = require('path');
@@ -14,7 +13,7 @@ var embedlr = require('gulp-embedlr'),
   express = require('express'),
   livereload = require('connect-livereload'),
   livereloadport = 35729,
-  serverport = 8888; 
+  serverport = 8080; 
 
 // Setup an express server with livereload
 var server = express();
@@ -29,7 +28,8 @@ gulp.task('dev', [
   'clean', 
   'views', 
   'styles', 
-  'js'
+  'js',
+  'copy-bower'
   ], function(){}
 );
 
@@ -37,7 +37,9 @@ gulp.task('dev', [
 gulp.task('clean', function(cb) {
   del([
     'dist/index.html',
-    // 'dist/css/*.css',
+    'dist/css/*.css',
+    'dist/fonts/*',
+    'dist/lib/*',
     'dist/js/*.js',
     'dist/views/*.html'
     ], cb);
@@ -52,19 +54,32 @@ gulp.task('views', function() {
 
 // Styles task
 gulp.task('styles', function() {
-  gulp.src('./app/less/*.less')
-    .pipe(less({
-      paths: [path.join(__dirname, 'less', 'includes')]
-    }))
-    .pipe(gulp.dest('./dist/css'));
+  gulp.src('app/assets/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('dist/css'));
 });
 
 // JS task
 gulp.task('js', function() {
-  gulp.src(['app/*.js'])
+  gulp.src('app/*.js')
     .pipe(gulp.dest('dist/js'));
 });
-  
+
+// Copy bower compenents
+gulp.task('copy-bower', function() {
+  var bc = 'bower_components/';
+  gulp.src([
+    bc + 'angular/angular.min.js', 
+    bc + 'angular-route/angular-route.min.js'
+  ]).pipe(gulp.dest('dist/lib'));
+
+  gulp.src([
+    bc + 'bootstrap/fonts/*', 
+    bc + 'fontawesome/fonts/*'
+  ]).pipe(gulp.dest('dist/fonts'));
+});
+
+// Start web server and watch files for changes  
 gulp.task('watch', function() {
   // Start webserver
   server.listen(serverport);
@@ -77,7 +92,7 @@ gulp.task('watch', function() {
   gulp.watch(['app/index.html', 'app/views/**/*.html'], [
     'views'
   ]);
-  gulp.watch(['app/less/*.less'], [
+  gulp.watch(['app/assets/less/*.less'], [
     'styles'
   ]);
   gulp.watch('./dist/**').on('change', refresh.changed);
