bd6e5fd4502ad4f1fadd9b1ce9c9c510512d6d9b
Kevin Parrott <parrott.kevin@gmail.com>
2014-10-26 22:39:45 -0400

    Added location information to page

--- src/app/components/info/info.controller.js
+++ src/app/components/info/info.controller.js
@@ -8,7 +8,31 @@
   InfoController.$inject = ['$scope', 'GoogleMapApi'.ns()];  
 
   function InfoController($scope) {
-    $scope.message = "I'm the info page (venue and hotel)";
+    $scope.title = "Information";
+
+    var locations = [{
+        title: 'Grand Rapids Art Museum',
+        address: '101 Monroe Center NW',
+        city: 'Grand Rapids',
+        state: 'MI',
+        zip: '49503',
+        link: 'http://www.artmuseumgr.org/',
+        latitude: 42.96505,
+        longitude: -85.67105
+      },
+      {
+        title: 'Courtyard Grand Rapids Downtown',
+        address: '11 Monroe NW',
+        city: 'Grand Rapids',
+        state: 'MI',
+        zip: '49503',
+        link: 'https://resweb.passkey.com/go/KevinandVirginia',
+        latitude: 42.964103,
+        longitude: -85.673271
+      }];
+
+    $scope.locations = locations;
+
     $scope.map = {
       center: {
         latitude: 42.96505,
@@ -16,21 +40,36 @@
       },
       zoom: 15
     };
+    
+    var markers = [];
+    for (var i = 0; i < locations.length; i++) {
+      markers.push(createMarker(i, locations[i]));
+    }
 
-    $scope.markers = [
-      {
-        id: 0,
-        title: 'gram',
-        latitude: 42.96505,
-        longitude: -85.67105
-      },
-      {
-        id: 1,
-        title: 'hotel',
-        latitude: 42.964103,
-        longitude: -85.673271  
-      }
-    ]; 
+    $scope.markers = markers;
+
+    function createMarker(i, location) {
+      var marker = {
+        id: i,
+        title: location.title,
+        address: location.address,
+        city: location.city,
+        state: location.state,
+        zip: location.zip,
+        link: location.link,
+        latitude: location.latitude,
+        longitude: location.longitude,
+        show: false,
+        onClick: function() {
+          $scope.show = !$scope.show;
+        }
+      };
+
+      return marker;
+    }
+
   }
+
+
 
 })();
