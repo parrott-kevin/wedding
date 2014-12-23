fff47d993d3dea86afdf881afa78658032d52451
Kevin Parrott <parrott.kevin@gmail.com>
2014-12-23 18:07:56 -0500

    Added Etsy to registry

--- src/app/components/registry/registry.controller.js
+++ src/app/components/registry/registry.controller.js
@@ -11,20 +11,25 @@
       var vm = this;
 
       vm.stores = [
-      {
-        name: 'West Elm',
-        pic: './assets/img/logo.west-elm.png',
-        link: 'https://secure.westelm.com/registry/n7rglc7vbt/registry-list.html'
-      },
-      {
-        name: 'Target',
-        pic: './assets/img/logo.target.png',
-        link: 'https://www-secure.target.com/gift-registry/giftgiver?registryId=UpMh6bDpPRmWALvOyN5jJg'
-      },
-      {
-        name: 'Williams and Sonoma',
-        pic: './assets/img/logo.williams-sonoma.svg',
-        link: 'https://secure.williams-sonoma.com/registry/c2pntv67zs/registry-list.html'
-      }];
+        {
+          name: 'West Elm',
+          pic: './assets/img/logo.west-elm.png',
+          link: 'https://secure.westelm.com/registry/n7rglc7vbt/registry-list.html'
+        },
+        {
+          name: 'Target',
+          pic: './assets/img/logo.target.png',
+          link: 'https://www-secure.target.com/gift-registry/giftgiver?registryId=UpMh6bDpPRmWALvOyN5jJg'
+        },
+        {
+          name: 'Williams and Sonoma',
+          pic: './assets/img/logo.williams-sonoma.svg',
+          link: 'https://secure.williams-sonoma.com/registry/c2pntv67zs/registry-list.html'
+        },
+        {
+          name: 'Etsy',
+          pic: './assets/img/logo.etsy.png',
+          link: 'https://www.etsy.com/registry/NDc2MTg1Mnw5MjM3OTYy/'
+        }];
     }
 })();
